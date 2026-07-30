3f63bf0911cf499b5c21570367592b933579fa1c
huntersoto <134533676+huntersoto@users.noreply.github.com>
2026-07-29 21:55:35 -0500

    Basic HTML and JS 1 assignment submission
    Basic HTML and JS 1 assignment submission

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Hello, World! I am an alert box!");
+
+function My_First_Function()	{
+				var String = "Kiss me, I'm Irish!";
+				var result = String.fontcolor("green");
+				documnet.getElementByID("Irish").innerHTML =
+				result;
+			};
